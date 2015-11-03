e3911b1f997609152c7ad9853e7690e91c9cc192
Johan Euphrosine <proppy@google.com>
2015-11-03 14:55:25 -0800

    update headers, add README

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
 # firebase-iot-button
+
+This sample show how to call firebase from an arduino sketch.
+
+*This is not an official Google product*.
+
+## Requirements
+
+- 1 esp8266
+- 1 attiny85
+- 1 push button
+- Arduino 1.6.x
+
+## Setup
+
+- Update `ssid` and `host` variable in `firebase.ino`.
+- Flash `firebase.ino` on the esp8266.
+- Flash `button.ino` on the attiny85.
+- Connect attiny softserial(0,1) on esp8266 rx/tx.
+
+## Provision
+- Generate a firebase token.
+- Send the following command over serial port on the esp8266:
+```
+TOKEN <firebase-token>
+```
+
+## Run
+- Open firebase dashboard on `/hits.json`.
+- Press the button.
+- Notice the new entry.
--- button.ino
+++ button.ino
@@ -1,9 +1,24 @@
- #include <SoftwareSerial.h>
+//
+// Copyright 2015 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+#include <SoftwareSerial.h>
 
 SoftwareSerial mySerial(1, 0); // RX, TX
 int pushButton = 2;
 
-// the setup routine runs once when you press reset:
 void setup() {
   mySerial.begin(115200);
   mySerial.println("hello");
@@ -22,6 +37,3 @@ void loop() {
   }
   previousState = buttonState;
 }
-
-
-
