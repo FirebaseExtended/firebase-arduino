076593e4e04b1a5ac4f2aaafc1c61800dae3175f
Ed Coyne <edcoyne@google.com>
2016-02-24 17:20:14 -0800

    re check in commands and add input stream

---
+++ modem/input-stream.h
@@ -0,0 +1,34 @@
+#ifndef MODEM_INPUT_STREAM_H
+#define MODEM_INPUT_STREAM_H
+
+namespace firebase {
+namespace modem {
+
+class InputStream {
+ public:
+  virtual String readLine() = 0;
+};
+
+class SerialInputStream : public InputStream {
+ public:
+  String readLine() override {
+    String out = Serial.readStringUntil('\r');
+    if (Serial.peek() == '\n') {
+      // This should be a '\n' so drop it.
+      Serial.read();
+    } else {
+      // Add \r back, this is not our endline.
+      out += '\r';
+
+      // Recurse until \r\n
+      out += readLine();
+    }
+    return out;
+  }
+};
+
+}  // modem
+}  // firebase
+
+#endif  // MODEM_INPUT_STREAM_H
+
