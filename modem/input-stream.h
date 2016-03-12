01d5860def5a2fe0fd902d13a5d893552ab5ebf9
Ed Coyne <edcoyne@google.com>
2016-03-11 16:13:33 -0800

    Add base for SerialTransciever.

--- modem/input-stream.h
+++ modem/input-stream.h
@@ -8,7 +8,40 @@ class InputStream {
  public:
   virtual String readLine() = 0;
   virtual String readStringUntil(const char terminator) = 0;
+  virtual void drain() = 0;
 };
+
+class ArduinoInputStream : public InputStream {
+ public:
+  ArduinoInputStream(Stream* stream) stream_(stream) {}
+  String readLine() {
+    String out = stream_->readStringUntil('\r');
+    if (stream_->peek() == '\n') {
+      // This should be a '\n' so drop it.
+      stream_->read();
+    } else {
+      // Add \r back, this is not our endline.
+      out += '\r';
+
+      // Recurse until \r\n
+      out += readLine();
+    }
+    return out;
+  }
+
+  String readStringUntil(const char terminator) {
+    return stream_->readStringUntil(terminator);
+  }
+
+  void drain() {
+    while(stream_->available()) {
+      stream_->read();
+    }
+  }
+
+ private:
+  Stream* stream_;
+}
 
 class SerialInputStream : public InputStream {
  public:
