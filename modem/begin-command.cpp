c9a98882ba55a4037c9e378d5180265b486a26c8
Ed Coyne <edcoyne@google.com>
2016-03-18 14:44:10 -0700

    changes to get SerialHost building

--- modem/begin-command.cpp
+++ modem/begin-command.cpp
@@ -26,17 +26,17 @@ bool BeginCommand::execute(const String& command,
     host = data;
   } else {
     // host and auth.
-    host = data.substr(0, space_index);
-    auth = data.substr(space_index + 1);
+    host = data.substring(0, space_index);
+    auth = data.substring(space_index + 1);
   }
 
-  if (host.empty()) {
+  if (host.length() != 0) {
     out->println("-FAIL Missing host");
     return false;
   }
 
   new_firebase_.reset(new Firebase(host));
-  if (!auth.empty()) {
+  if (auth.length() != 0) {
     new_firebase_->auth(auth);
   }
 
