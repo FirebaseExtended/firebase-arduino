461451434d9e341927c374362cf8cb99b93afeac
Johan Euphrosine <proppy@google.com>
2016-12-16 22:17:22 -0800

    contrib/test: fix snippets path

--- contrib/test/travis/check_all_examples_use_standard_init.sh
+++ contrib/test/travis/check_all_examples_use_standard_init.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-FBASE_SNIPPET=test/travis/firebase_init.cc.snip
-FBASE_NOAUTH_SNIPPET=test/travis/no_firebase_init.cc.snip
-FBASE_NOCONFIG_SNIPPET=test/travis/no_config_init.cc.snip
-for example in `find examples/ -name *.ino`;
+FBASE_SNIPPET=contrib/test/travis/firebase_init.cc.snip
+FBASE_NOAUTH_SNIPPET=contrib/test/travis/no_firebase_init.cc.snip
+FBASE_NOCONFIG_SNIPPET=contrib/test/travis/no_config_init.cc.snip
+for example in `find examples/ contrib/examples/ -name *.ino`;
 do
   echo $example;
   (xxd -p $example | tr -d '\n' | grep -q `xxd -p $FBASE_SNIPPET | tr -d '\n'`) ||
@@ -10,7 +10,7 @@ do
   (xxd -p $example | tr -d '\n' | grep -q `xxd -p $FBASE_NOCONFIG_SNIPPET | tr -d '\n'`) ||
   if [ $? -ne 0 ];
   then
-    echo $example does not contain standard defined in test/travis/*_init.cc.snip.
+    echo $example does not contain standard defined in contrib/test/travis/*_init.cc.snip.
     exit 1;
   fi;
 done;
