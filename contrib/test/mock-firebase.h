82cd290b5092a8f34ae0f0266c052c45ad82c7e7
Sergei Kotlyachkov <kotl@2x2.cc>
2018-04-18 00:35:40 -0400

    fix param definition in mock-firebase.h

--- contrib/test/mock-firebase.h
+++ contrib/test/mock-firebase.h
@@ -11,13 +11,13 @@ namespace modem {
 class MockFirebase : public FirebaseArduino {
  public:
   MOCK_CONST_METHOD0(error, const FirebaseError&());
-  MOCK_CONST_METHOD1(getString, String ());
-  MOCK_CONST_METHOD2(pushString, void());
-  MOCK_CONST_METHOD1(remove, void());
-  MOCK_CONST_METHOD2(setString, void());
+  MOCK_CONST_METHOD1(getString, String (const String& path));
+  MOCK_CONST_METHOD2(pushString, void(const String& path, const String& data));
+  MOCK_CONST_METHOD1(remove, void(const String& path));
+  MOCK_CONST_METHOD2(setString, void(const String& path, const String& data));
   MOCK_CONST_METHOD0(available, bool ());
   MOCK_CONST_METHOD0(readEvent, FirebaseObject ());
-  MOCK_CONST_METHOD2(begin, void ());
+  MOCK_CONST_METHOD2(begin, void (const String& host, const String& auth));
 };
 
 
