e62c2177aadb891d7ef30d320ebcb9a32db6aba6
Alexandre Capt <acapt@adobe.com>
2018-08-08 15:11:52 +0200

    Firebase.readEvent() returns "type" instead "put" while getting data from Firebase

--- src/FirebaseArduino.cpp
+++ src/FirebaseArduino.cpp
@@ -179,7 +179,7 @@ FirebaseObject FirebaseArduino::readEvent() {
   String event = client->readStringUntil('\n').substring(6);
   client->readStringUntil('\n'); // consume separator
   FirebaseObject obj = FirebaseObject(event.c_str());
-  obj.getJsonVariant().asObject()["type"] = type.c_str();
+  obj.getJsonVariant().asObject()["type"] = type;
   return obj;
 }
 
